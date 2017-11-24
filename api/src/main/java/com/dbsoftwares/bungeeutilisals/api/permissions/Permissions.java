7c3ff247e5a472542b151816006e93d4594d8b86
Dieter Blancke <dieter08@live.be>
2017-11-24 18:45:01 +0100

    updated permissions, started ban command

--- api/src/main/java/com/dbsoftwares/bungeeutilisals/api/permissions/Permissions.java
+++ api/src/main/java/com/dbsoftwares/bungeeutilisals/api/permissions/Permissions.java
@@ -6,10 +6,14 @@ import lombok.experimental.UtilityClass;
 public class Permissions {
 
     /* Bypass Permissions */
-    private String BYPASS_PREFIX = "bungeeutilisals.bypass.";
+    private String BUNGEEUTILISALS = "bungeeutilisals.";
+    private String BYPASS_PREFIX = BUNGEEUTILISALS + "bypass.";
+
     public String AD_BYPASS = BYPASS_PREFIX + "antiad";
     public String CAPS_BYPASS = BYPASS_PREFIX + "anticaps";
     public String SPAM_BYPASS = BYPASS_PREFIX + "antispam";
     public String SWEAR_BYPASS = BYPASS_PREFIX + "antiswear";
 
+    private String PUNISHMENT_PREFIX = BUNGEEUTILISALS + "punishments.";
+    public String BAN_PERMISSION = PUNISHMENT_PREFIX + "ban";
 }
--- core/src/main/java/com.dbsoftwares.bungeeutilisals/bungee/punishments/commands/BanCommand.java
+++ core/src/main/java/com.dbsoftwares.bungeeutilisals/bungee/punishments/commands/BanCommand.java
@@ -1,15 +1,18 @@
 package com.dbsoftwares.bungeeutilisals.bungee.punishments.commands;
 
 import com.dbsoftwares.bungeeutilisals.api.command.BUCommand;
+import com.dbsoftwares.bungeeutilisals.api.permissions.Permissions;
 import com.dbsoftwares.bungeeutilisals.api.user.User;
+import com.dbsoftwares.bungeeutilisals.bungee.BungeeUtilisals;
+import com.google.common.collect.Lists;
 import net.md_5.bungee.api.CommandSender;
 
 import java.util.List;
 
 public class BanCommand extends BUCommand {
 
-    public BanCommand(String name, List<String> aliases, String permission) {
-        super(name, aliases, permission);
+    public BanCommand() {
+        super("ban", BungeeUtilisals.getInstance().getAliases().getOrDefault("ban", Lists.newArrayList()), Permissions.BAN_PERMISSION);
     }
 
     @Override
@@ -19,6 +22,10 @@ public class BanCommand extends BUCommand {
 
     @Override
     public void onExecute(User user, String[] args) {
+        if (args.length < 2) {
+            // TODO: send usage message to user
+            return;
+        }
 
     }
 
