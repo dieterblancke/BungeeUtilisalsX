2f1d31416a46ec731ecabd606ee85b386d4d13b0
didjee2 <dieter08@live.be>
2018-08-24 12:28:21 +0200

    working on chatlock command

--- core/src/main/java/com/dbsoftwares/bungeeutilisals/commands/general/ChatLockCommand.java
+++ core/src/main/java/com/dbsoftwares/bungeeutilisals/commands/general/ChatLockCommand.java
@@ -7,17 +7,27 @@ package com.dbsoftwares.bungeeutilisals.commands.general;
  */
 
 import com.dbsoftwares.bungeeutilisals.BungeeUtilisals;
+import com.dbsoftwares.bungeeutilisals.api.BUCore;
 import com.dbsoftwares.bungeeutilisals.api.command.Command;
 import com.dbsoftwares.bungeeutilisals.api.user.interfaces.User;
 import com.dbsoftwares.bungeeutilisals.api.utils.file.FileLocation;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.config.ServerInfo;
+import net.md_5.bungee.api.event.ChatEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.event.EventHandler;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
-public class ChatLockCommand extends Command {
+public class ChatLockCommand extends Command implements Listener {
+
+    private static final List<String> lockedChatServers = Lists.newArrayList();
 
     public ChatLockCommand() {
         super(
@@ -25,6 +35,25 @@ public class ChatLockCommand extends Command {
                 Arrays.asList(FileLocation.GENERALCOMMANDS.getConfiguration().getString("chatlock.aliases").split(", ")),
                 FileLocation.GENERALCOMMANDS.getConfiguration().getString("chatlock.permission")
         );
+
+        ProxyServer.getInstance().getPluginManager().registerListener(BungeeUtilisals.getInstance(), this);
+    }
+
+    public static void lockChat(String server, String by) {
+        ServerInfo serverInfo = ProxyServer.getInstance().getServerInfo(server);
+        Stream<User> users = server.equals("ALL")
+                ? BUCore.getApi().getUsers().stream()
+                : BUCore.getApi().getUsers().stream().filter(u -> u.getServerName().equalsIgnoreCase(server));
+
+        if (lockedChatServers.contains(server)) {
+            lockedChatServers.remove(server);
+
+            users.forEach(u -> u.sendLangMessage("general-commands.chatlock.unlocked", "{user}", by));
+        } else {
+            lockedChatServers.add(server);
+
+            users.forEach(u -> u.sendLangMessage("general-commands.chatlock.locked", "{user}", by));
+        }
     }
 
     @Override
@@ -45,6 +74,14 @@ public class ChatLockCommand extends Command {
         } else {
             // TODO: Lock chat
         }
+    }
+
+    @EventHandler
+    public void onChat(ChatEvent event) {
+        if (event.isCommand() || event.isCancelled()) {
+            return;
+        }
+        // TODO
     }
 
     @Data
