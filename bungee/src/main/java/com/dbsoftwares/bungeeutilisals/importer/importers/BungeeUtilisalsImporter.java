fdb49dffd391318ff0e2bfe581e2916f3fa7821a
Dieter Blancke <dieter08@live.be>
2018-08-27 13:46:03 +0200

    working on old BungeeUtilisals importer
    Signed-off-by: Dieter Blancke <dieter08@live.be>

--- bungee/src/main/java/com/dbsoftwares/bungeeutilisals/importer/importers/BungeeUtilisalsImporter.java
+++ bungee/src/main/java/com/dbsoftwares/bungeeutilisals/importer/importers/BungeeUtilisalsImporter.java
@@ -5,10 +5,7 @@ import com.dbsoftwares.bungeeutilisals.importer.ImporterCallback;
 import com.google.common.collect.Lists;
 import com.zaxxer.hikari.HikariDataSource;
 
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Map;
 
 public class BungeeUtilisalsImporter extends Importer {
@@ -22,15 +19,31 @@ public class BungeeUtilisalsImporter extends Importer {
             connection.setAutoCommit(false);
 
             final DatabaseMetaData databaseMetaData = connection.getMetaData();
-            for (final String table : Lists.newArrayList("Bans", "IPBans", "Mutes")) {
+            for (final String table : Lists.newArrayList("PlayerInfo", "Bans", "IPBans", "Mutes")) {
                 final ResultSet tables = databaseMetaData.getTables(null, null, table, null);
                 if (!tables.next()) {
                     throw new IllegalArgumentException("Could not find table " + table + ", stopping import ...");
                 }
             }
 
-            // TODO: Import
+            status = new ImporterStatus(0 /* Make counter for entries in all tables together */);
+
+            // TODO: Import | MAKE METHOD FOR EACH TABLE (Bans, Mutes, IPBans)
             int uncommited = 0;
+            try (PreparedStatement pstmt = connection.prepareStatement("SELECT * FROM Bans;");
+                 ResultSet rs = pstmt.executeQuery()) {
+                while (rs.next()) {
+
+                    uncommited++;
+
+                    if (uncommited % 100 == 0) {
+                        uncommited = 0;
+                        connection.commit();
+                        status.incrementConvertedEntries(uncommited);
+                        importerCallback.onStatusUpdate(status);
+                    }
+                }
+            }
 
             connection.commit();
         } catch (SQLException e) {
