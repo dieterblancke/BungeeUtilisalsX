08d4b226555051b2506fd61354267e20f137f1a3
Dieter Blancke <dieter08@live.be>
2018-09-08 10:41:11 +0200

    finished Mongo Friends Dao, TODO: testing
    Signed-off-by: Dieter Blancke <dieter08@live.be>

--- bungee/src/main/java/com/dbsoftwares/bungeeutilisals/storage/data/mongo/dao/MongoFriendsDao.java
+++ bungee/src/main/java/com/dbsoftwares/bungeeutilisals/storage/data/mongo/dao/MongoFriendsDao.java
@@ -5,9 +5,16 @@ import com.dbsoftwares.bungeeutilisals.api.friends.FriendData;
 import com.dbsoftwares.bungeeutilisals.api.placeholder.PlaceHolderAPI;
 import com.dbsoftwares.bungeeutilisals.api.storage.dao.FriendsDao;
 import com.dbsoftwares.bungeeutilisals.api.user.interfaces.User;
+import com.dbsoftwares.bungeeutilisals.storage.mongodb.Mapping;
 import com.dbsoftwares.bungeeutilisals.storage.mongodb.MongoDBStorageManager;
+import com.google.common.collect.Lists;
+import com.mongodb.Block;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import org.bson.Document;
 
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
@@ -19,21 +26,47 @@ import java.util.UUID;
 
 public class MongoFriendsDao implements FriendsDao {
 
-    // TODO: create mongo friends dao, maybe other storage structure?
-
     @Override
     public void addFriend(User user, UUID uuid) {
+        final Mapping<String, Object> mapping = new Mapping<>(true);
 
+        mapping.append("user", user.getUUID());
+        mapping.append("friend", uuid);
+        mapping.append("friendsince", new Date(System.currentTimeMillis()));
+
+        getDatabase().getCollection(format("{friends-table}")).insertOne(new Document(mapping.getMap()));
     }
 
     @Override
     public void removeFriend(User user, UUID uuid) {
+        final MongoCollection<Document> coll = getDatabase().getCollection(format("{friends-table}"));
 
+        coll.deleteOne(
+                Filters.and(
+                        Filters.eq("user", user.getUUID()),
+                        Filters.eq("friend", uuid)
+                )
+        );
     }
 
     @Override
     public List<FriendData> getFriends(UUID uuid) {
-        return null;
+        final List<FriendData> friends = Lists.newArrayList();
+        final MongoCollection<Document> coll = getDatabase().getCollection(format("{friends-table}"));
+        final MongoCollection<Document> userColl = getDatabase().getCollection(format("{users-table}"));
+
+        coll.find(Filters.eq("user", uuid)).forEach((Block<? super Document>) doc -> {
+            final Document friend = userColl.find(Filters.eq("user", doc.getString("friend"))).first();
+
+            friends.add(new FriendData(
+                    uuid,
+                    friend.getString("name"),
+                    doc.getDate("friendsince"),
+                    friend.getDate("lastlogout")
+            ));
+        });
+
+        return friends;
     }
 
     private String format(String line) {
