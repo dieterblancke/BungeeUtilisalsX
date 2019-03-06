aa036429ecf3e6e1dea47fee668a95348fdebdb9
Dieter Blancke <dieter.blancke@outlook.com>
2019-03-06 16:07:23 +0100

    The UUID api that was being used has been removed, so using a new one for now. This can now also be changed in the config (for people who want to use a custom API for example)
    Signed-off-by: Dieter Blancke <dieter.blancke@outlook.com>

--- bungee/src/main/java/com/dbsoftwares/bungeeutilisals/utils/MojangUtils.java
+++ bungee/src/main/java/com/dbsoftwares/bungeeutilisals/utils/MojangUtils.java
@@ -21,7 +21,6 @@ package com.dbsoftwares.bungeeutilisals.utils;
 import com.dbsoftwares.bungeeutilisals.BungeeUtilisals;
 import com.dbsoftwares.bungeeutilisals.api.BUCore;
 import com.google.gson.Gson;
-import lombok.Data;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -32,16 +31,11 @@ import java.util.UUID;
 
 public class MojangUtils {
 
-    //private static final String UUID_URL = "https://api.mojang.com/users/profiles/minecraft/";
-    //private static final String NAME_URL = "https://sessionserver.mojang.com/session/minecraft/profile/";
-
-    private static final String UUID_URL = "https://ss.gameapis.net/uuid/";
-    private static final String NAME_URL = "https://ss.gameapis.net/name/";
     private static final Gson gson = new Gson();
 
     public static String getUUID(final String name) {
         try {
-            final URL url = new URL(UUID_URL + name);
+            final URL url = new URL(getNameToUuidUrl() + name);
             final HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
             conn.setRequestProperty("User-Agent", "BungeeUtilisals/" + BungeeUtilisals.getInstance().getDescription().getVersion());
@@ -50,8 +44,8 @@ public class MojangUtils {
                  final InputStreamReader isr = new InputStreamReader(input)) {
                 final MojangProfile profile = gson.fromJson(isr, MojangProfile.class);
 
-                if (profile != null && !profile.uuid_formatted.isEmpty()) {
-                    return profile.uuid_formatted;
+                if (profile != null) {
+                    return profile.getUuid();
                 }
             }
         } catch (final IOException e) {
@@ -63,7 +57,7 @@ public class MojangUtils {
 
     public static String getName(final UUID uuid) {
         try {
-            final URL url = new URL(NAME_URL + uuid.toString().replace("-", ""));
+            final URL url = new URL(getUuidToNameUrl() + uuid.toString().replace("-", ""));
             final HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
             conn.setRequestProperty("User-Agent", "BungeeUtilisals/" + BungeeUtilisals.getInstance().getDescription().getVersion());
@@ -72,8 +66,8 @@ public class MojangUtils {
                  final InputStreamReader isr = new InputStreamReader(input)) {
                 final MojangProfile profile = gson.fromJson(isr, MojangProfile.class);
 
-                if (profile != null && !profile.name.isEmpty()) {
-                    return profile.name;
+                if (profile != null) {
+                    return profile.getName();
                 }
             }
         } catch (final IOException e) {
@@ -83,12 +77,36 @@ public class MojangUtils {
         return null;
     }
 
-    @Data
+    private static String getNameToUuidUrl() {
+        return BungeeUtilisals.getInstance().getConfig().get("urls.name-to-uuid", "https://api.minetools.eu/uuid/");
+    }
+
+    private static String getUuidToNameUrl() {
+        return BungeeUtilisals.getInstance().getConfig().get("urls.uuid-to-name", "https://api.minetools.eu/uuid/");
+    }
+
     private class MojangProfile {
 
         private String id;
         private String name;
+        private String uuid;
         private String uuid_formatted;
 
+        public String getName() {
+            return name;
+        }
+
+        public String getUuid() {
+            if (id != null) {
+                return id;
+            }
+            if (uuid != null) {
+                return uuid;
+            }
+            if (uuid_formatted != null) {
+                return uuid_formatted;
+            }
+            return null;
+        }
     }
 }
