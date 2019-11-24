1a312413239fd9b8aac7dd9cbcfd24c56acc41a0
Dieter Blancke <dieter.blancke@outlook.com>
2019-11-24 20:31:08 +0100

    Fixed antiadvertising for web urls that contain capital letters
    Signed-off-by: Dieter Blancke <dieter.blancke@outlook.com>

--- bungee/src/main/java/com/dbsoftwares/bungeeutilisals/manager/ChatManager.java
+++ bungee/src/main/java/com/dbsoftwares/bungeeutilisals/manager/ChatManager.java
@@ -37,7 +37,7 @@ public class ChatManager implements IChatManager
 {
 
     private Pattern ippattern = Pattern.compile( "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$" );
-    private Pattern webpattern = Pattern.compile( "^((https?|ftp)://|(www|ftp)\\.)?[a-z0-9-]+(\\.[a-z0-9-]+)+([/?].*)?$" );
+    private Pattern webpattern = Pattern.compile( "^((https?|ftp)://|(www|ftp)\\.)?[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)+([/?].*)?$" );
 
     private List<Pattern> swearPatterns = Lists.newArrayList();
     private Map<String, String> utfSymbols = Maps.newHashMap();
