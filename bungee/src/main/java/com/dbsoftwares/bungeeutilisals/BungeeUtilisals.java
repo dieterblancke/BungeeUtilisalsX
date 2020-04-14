ec0edc5bb0ceffa2cd6c513279c79eb2e3dfa86d
Dieter Blancke <dieter.blancke@outlook.com>
2020-04-14 15:12:40 +0200

    Forgot to remove the BUCommand initializer
    Signed-off-by: Dieter Blancke <dieter.blancke@outlook.com>

--- bungee/src/main/java/com/dbsoftwares/bungeeutilisals/BungeeUtilisals.java
+++ bungee/src/main/java/com/dbsoftwares/bungeeutilisals/BungeeUtilisals.java
@@ -547,7 +547,6 @@ public class BungeeUtilisals extends Plugin
         loadGeneralCommand( "clearchat", ClearChatCommand.class );
         loadGeneralCommand( "chatlock", ChatLockCommand.class );
         loadGeneralCommand( "glag", GLagCommand.class );
-        loadGeneralCommand( "staffchat", StaffChatCommandCall.class );
         loadGeneralCommand( "language", LanguageCommand.class );
         loadGeneralCommand( "staff", StaffCommand.class );
         loadGeneralCommand( "msg", MsgCommand.class );
