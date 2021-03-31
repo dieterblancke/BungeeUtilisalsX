2e84e84263f9519d45ca1804ebb681f7750f8b9a
Dieter Blancke <dieter.blancke@outlook.com>
2021-03-31 23:38:02 +0200

    Fixed table in migration and added indexes.

--- common/src/main/resources/migrations/v3_add_punishment_tracks.sql
+++ common/src/main/resources/migrations/v3_add_punishment_tracks.sql
@@ -4,14 +4,14 @@ CREATE TABLE IF NOT EXISTS {punishmenttracks-table}
     uuid                    DATA_TYPE_VARCHAR  NOT NULL,
     track_id                DATA_TYPE_VARCHAR  NOT NULL,
     server                  DATA_TYPE_VARCHAR  NOT NULL,
+    executed_by             DATA_TYPE_VARCHAR  NOT NULL,
     date                    DATA_TYPE_DATETIME NOT NULL,
     active                  DATA_TYPE_BOOLEAN  NOT NULL,
-    executed_by             DATA_TYPE_VARCHAR  NOT NULL,
-    duration                DATA_TYPE_BIGINT   NOT NULL,
-    type                    DATA_TYPE_VARCHAR  NOT NULL,
-    removed                 DATA_TYPE_BOOLEAN  NOT NULL DEFAULT_VALUE_BOOLEAN_FALSE,
-    removed_by              DATA_TYPE_VARCHAR,
-    removed_at              DATA_TYPE_DATETIME NULL DEFAULT NULL,
-    punishmentaction_status DATA_TYPE_BOOLEAN  NOT NULL DEFAULT_VALUE_BOOLEAN_FALSE,
     PRIMARY KEY (id)
 );
+
+CREATE INDEX idx_tracks_e ON {punishmenttracks-table} (executed_by);
+
+CREATE INDEX idx_tracks_u ON {punishmenttracks-table} (uuid);
+CREATE INDEX idx_tracks_ua ON {punishmenttracks-table} (uuid, active);
+CREATE INDEX idx_tracks_uas ON {punishmenttracks-table} (uuid, active, server);
