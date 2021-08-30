d3dc0c73377a193e3e578e163305246f2070e637
Dieter Blancke <dieter.blancke@outlook.com>
2021-08-30 19:10:43 +0200

    renamed migration

--- common/src/main/java/be/dieterblancke/bungeeutilisalsx/common/migration/sql/migrations/v6_api_tokens.java
+++ common/src/main/java/be/dieterblancke/bungeeutilisalsx/common/migration/sql/migrations/v6_api_tokens.java
@@ -2,12 +2,12 @@ package be.dieterblancke.bungeeutilisalsx.common.migration.sql.migrations;
 
 import be.dieterblancke.bungeeutilisalsx.common.migration.FileMigration;
 
-public class v6_api_keys extends FileMigration
+public class v6_api_tokens extends FileMigration
 {
 
-    public v6_api_keys()
+    public v6_api_tokens()
     {
-        super( "migrations/v6_api_keys.sql" );
+        super( "migrations/v6_api_tokens.sql" );
     }
 
     @Override
