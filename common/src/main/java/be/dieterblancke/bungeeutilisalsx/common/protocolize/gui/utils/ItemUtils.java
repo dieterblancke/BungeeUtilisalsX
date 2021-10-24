2c95fd153e03398a0bc2cec9cece8f9cc1cbf412
Dieter Blancke <dieter.blancke@outlook.com>
2021-10-24 22:51:07 +0200

    Fixed skull texture

--- common/src/main/java/be/dieterblancke/bungeeutilisalsx/common/protocolize/gui/utils/ItemUtils.java
+++ common/src/main/java/be/dieterblancke/bungeeutilisalsx/common/protocolize/gui/utils/ItemUtils.java
@@ -4,25 +4,24 @@ import dev.simplix.protocolize.api.item.ItemStack;
 import net.querz.nbt.tag.CompoundTag;
 import net.querz.nbt.tag.ListTag;
 
-import java.util.UUID;
-
 public class ItemUtils
 {
 
-    public static void setSkullTexture( final ItemStack itemStack, final String texture )
+    public static void setSkullTexture( final ItemStack itemStack, final String textureUrl )
     {
-        final CompoundTag compoundTag = itemStack.nbtData();
-        final CompoundTag skull = new CompoundTag();
-        final CompoundTag skullProperties = new CompoundTag();
-        final ListTag<CompoundTag> skullTextures = new ListTag<>( CompoundTag.class );
-        final CompoundTag textureTag = new CompoundTag();
+        final CompoundTag skullOwner = new CompoundTag();
 
-        textureTag.putString( "Value", texture );
-        skullTextures.add( textureTag );
-        skullProperties.put( "textures", skullTextures );
+        skullOwner.putString( "Name", textureUrl );
+        final CompoundTag properties = new CompoundTag();
 
-        skull.putString( "Id", UUID.randomUUID().toString() );
-        skull.put( "Properties", skullProperties );
-        compoundTag.put( "SkullOwner", skull );
+        final ListTag<CompoundTag> textures = new ListTag<>( CompoundTag.class );
+        final CompoundTag texture = new CompoundTag();
+
+        texture.putString( "Value", textureUrl );
+        textures.add( texture );
+
+        properties.put( "textures", textures );
+        skullOwner.put( "Properties", properties );
+        itemStack.nbtData().put( "SkullOwner", skullOwner );
     }
 }
