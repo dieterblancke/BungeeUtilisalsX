47f3680da7ea99f84967c10b68263eb2e3ad1eb5
Dieter Blancke <dieter.blancke@outlook.com>
2021-11-21 23:34:40 +0100

    Fixed {user-prefix} placeholder

--- common/src/main/java/be/dieterblancke/bungeeutilisalsx/common/api/utils/StaffUtils.java
+++ common/src/main/java/be/dieterblancke/bungeeutilisalsx/common/api/utils/StaffUtils.java
@@ -2,11 +2,16 @@ package be.dieterblancke.bungeeutilisalsx.common.api.utils;
 
 import be.dieterblancke.bungeeutilisalsx.common.BuX;
 import be.dieterblancke.bungeeutilisalsx.common.api.user.interfaces.User;
+import be.dieterblancke.bungeeutilisalsx.common.api.utils.config.ConfigFiles;
+import be.dieterblancke.bungeeutilisalsx.common.api.utils.other.StaffRankData;
 import be.dieterblancke.bungeeutilisalsx.common.api.utils.other.StaffUser;
+import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
 
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 public class StaffUtils
 {
@@ -39,5 +44,14 @@ public class StaffUtils
             }
         }
         return BuX.getApi().getUser( name ).map( User::isVanished ).orElse( false );
+    }
+
+    public static Optional<StaffRankData> getStaffRankForUser( final User user) {
+        final String group = BuX.getInstance().getActivePermissionIntegration().getGroup( user.getUuid() );
+
+        return ConfigFiles.RANKS.getRanks()
+                .stream()
+                .filter( rank -> Strings.isNullOrEmpty( group ) ? user.hasPermission( rank.getPermission(), true ) : rank.getName().equalsIgnoreCase( group ) )
+                .max( Comparator.comparingInt( StaffRankData::getPriority ) );
     }
 }
