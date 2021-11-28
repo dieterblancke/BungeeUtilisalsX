7b71f48232b2286ec65ff973ed9e0f628594d736
Dieter Blancke <dieter.blancke@outlook.com>
2021-11-28 12:09:15 +0100

    Finished party info command

--- common/src/main/java/be/dieterblancke/bungeeutilisalsx/common/commands/party/sub/PartyInfoSubCommandCall.java
+++ common/src/main/java/be/dieterblancke/bungeeutilisalsx/common/commands/party/sub/PartyInfoSubCommandCall.java
@@ -21,12 +21,13 @@ package be.dieterblancke.bungeeutilisalsx.common.commands.party.sub;
 import be.dieterblancke.bungeeutilisalsx.common.BuX;
 import be.dieterblancke.bungeeutilisalsx.common.api.command.CommandCall;
 import be.dieterblancke.bungeeutilisalsx.common.api.party.Party;
-import be.dieterblancke.bungeeutilisalsx.common.api.user.UserStorage;
+import be.dieterblancke.bungeeutilisalsx.common.api.party.PartyMember;
 import be.dieterblancke.bungeeutilisalsx.common.api.user.interfaces.User;
-import be.dieterblancke.bungeeutilisalsx.common.api.utils.UserUtils;
+import be.dieterblancke.bungeeutilisalsx.common.api.utils.Utils;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class PartyInfoSubCommandCall implements CommandCall
 {
@@ -34,7 +35,24 @@ public class PartyInfoSubCommandCall implements CommandCall
     @Override
     public void onExecute( final User user, final List<String> args, final List<String> parameters )
     {
-        // TODO
+        final Optional<Party> optionalParty = BuX.getInstance().getPartyManager().getCurrentPartyFor( user.getName() );
+
+        if ( !optionalParty.isPresent() )
+        {
+            user.sendLangMessage( "party.not-in-party" );
+            return;
+        }
+        final Party party = optionalParty.get();
+
+        user.sendLangMessage(
+                "party.info",
+                "{party-owner}", party.getOwner().getNickName(),
+                "{party-created-at}", Utils.formatDate( party.getCreatedAt(), user.getLanguageConfig().getConfig() ),
+                "{party-member-count}", party.getPartyMembers().size(),
+                "{party-member-list}", party.getPartyMembers().stream().map( PartyMember::getNickName ).collect( Collectors.joining( ", " ) ),
+                "{party-invitation-count}", party.getSentInvites().size(),
+                "{party-joinrequest-count}", party.getJoinRequests().size()
+        );
     }
 
     @Override
